1a46e794ffa49c6ef0c3da1a23050a259ff55aa1
festus84 <festusaakinyemi84@gmail.com>
2023-01-13 16:35:38 +0100

    directory.py

---
+++ directory.py
@@ -0,0 +1,6 @@
+import os
+import sys
+print("Current Working directory", os.getcwd())
+path = "C:/Users/FESTUS AKINYEMI/PycharmProjects/pythonLearn/Hacathon"
+os.chdir(path)
+print("Current Working directory", os.getcwd())
